19f1e1cc19a61500b085b1e87c42892f48715eff
Sebastian Morales Duque <91298191+SebastianMoralesDuque@users.noreply.github.com>
2022-05-12 00:17:20 -0500

    Add files via upload

---
+++ Seguimiento/7/script2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+function httpd {
+
+   sudo yum remove nginx -y
+   sudo yum install httpd -y
+   sudo systemctl start httpd
+   sudo systemctl enable httpd
+}
+
+function nginx {
+   sudo yum remove httpd -y
+   sudo yum install nginx -y
+   sudo systemctl start nginx
+   sudo systemctl enable nginx
+
+}
+
+
+
+function main {
+   if [[ "$parametro" == "nginx" ]]
+   then
+      httpd
+   else
+      nginx
+   fi
+}
+main $parametro
+
